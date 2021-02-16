57a5dfb604c306b2827e3fa07356b68bbd5c7442
Jay Gholizadeh <jgolizadeh@gmail.com>
2021-02-16 13:10:34 +0000

    feat: new notebook file and write the newton_method

--- Newton.py
+++ Newton.py
@@ -2,5 +2,19 @@ import pandas as pd
 import numpy as np
 
 def f_derivative(f,x):
+    h = 1/1000
 
-    return f(x+)
+    return (f(x+h)-f(x))/h
+def f(x):
+    return x**2-100
+
+def f_derivative(f,x):
+    h = 1/1000
+    return (f(x+h)-f(x))/h
+
+x0 = 0
+def Newton_method(x0):
+    x_prime = x0
+    for i in np.arange(20):
+        x_prime = x_prime - (f(x_prime)/f_derivative(f,x_prime))
+    return x_prime
